89caa76bd468c26309f13115e4519ea2ccec893f
Roksolyana <RoksolyanaLebedenko>
2017-06-19 13:40:22 +0300

    Web-server

---
+++ web_server.cpp
@@ -0,0 +1,154 @@
+#include "server_http.hpp"
+#include "client_http.hpp"
+
+//Added for the json-example
+#define BOOST_SPIRIT_THREADSAFE
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/json_parser.hpp>
+
+//Added for the default_resource example
+#include <fstream>
+#include <boost/filesystem.hpp>
+#include <vector>
+#include <algorithm>
+#ifdef HAVE_OPENSSL
+#include "crypto.hpp"
+#endif
+
+using namespace std;
+//Added for the json-example:
+using namespace boost::property_tree;
+
+typedef SimpleWeb::Server<SimpleWeb::HTTP> HttpServer;
+typedef SimpleWeb::Client<SimpleWeb::HTTP> HttpClient;
+
+//Added for the default_resource example
+void default_resource_send(const HttpServer &server, const shared_ptr<HttpServer::Response> &response,
+                           const shared_ptr<ifstream> &ifs);
+
+int main() {
+    //HTTP-server at port 8080 
+    HttpServer server;
+    server.config.port=8080;
+    
+    //Add resources using path-regex and method-string, and an anonymous function
+    //POST-example for the path /string, responds the posted string
+    server.resource["^/string$"]["POST"]=[](shared_ptr<HttpServer::Response> response, shared_ptr<HttpServer::Request> request) {
+        //Retrieve string:
+        auto content=request->content.string();
+        //request->content.string() is a convenience function for:
+        //stringstream ss;
+        //ss << request->content.rdbuf();
+        //string content=ss.str();
+        
+        *response << "HTTP/1.1 200 OK\r\nContent-Length: " << content.length() << "\r\n\r\n" << content;
+    };
+
+
+    //GET-example for the path /info
+    //Responds with request-information
+    server.resource["^/info$"]["GET"]=[](shared_ptr<HttpServer::Response> response, shared_ptr<HttpServer::Request> request) {
+        stringstream content_stream;
+        content_stream << "<h1>Request from " << request->remote_endpoint_address << " (" << request->remote_endpoint_port << ")</h1>";
+        content_stream << request->method << " " << request->path << " HTTP/" << request->http_version << "<br>";
+        for(auto& header: request->header) {
+            content_stream << header.first << ": " << header.second << "<br>";
+        }
+        
+        //find length of content_stream (length received using content_stream.tellp())
+        content_stream.seekp(0, ios::end);
+        
+        *response <<  "HTTP/1.1 200 OK\r\nContent-Length: " << content_stream.tellp() << "\r\n\r\n" << content_stream.rdbuf();
+    };
+    
+    //GET-example for the path /match/[number], responds with the matched string in path (number)
+    //For instance a request GET /match/123 will receive: 123
+    server.resource["^/match/([0-9]+)$"]["GET"]=[](shared_ptr<HttpServer::Response> response, shared_ptr<HttpServer::Request> request) {
+        string number=request->path_match[1];
+        *response << "HTTP/1.1 200 OK\r\nContent-Length: " << number.length() << "\r\n\r\n" << number;
+    };
+    
+    //Get example simulating heavy work in a separate thread
+    server.resource["^/work$"]["GET"]=[](shared_ptr<HttpServer::Response> response, shared_ptr<HttpServer::Request> /*request*/) {
+        thread work_thread([response] {
+            this_thread::sleep_for(chrono::seconds(5));
+            string message="Work done";
+            *response << "HTTP/1.1 200 OK\r\nContent-Length: " << message.length() << "\r\n\r\n" << message;
+        });
+        work_thread.detach();
+    };
+    
+    //Default GET-example. If no other matches, this anonymous function will be called. 
+    //Will respond with content in the web/-directory, and its subdirectories.
+    //Default file: index.html
+    //Can for instance be used to retrieve an HTML 5 client that uses REST-resources on this server
+    server.default_resource["GET"]=[&server](shared_ptr<HttpServer::Response> response, shared_ptr<HttpServer::Request> request) {
+        try {
+            auto web_root_path=boost::filesystem::canonical("web");
+            auto path=boost::filesystem::canonical(web_root_path/request->path);
+            //Check if path is within web_root_path
+            if(distance(web_root_path.begin(), web_root_path.end())>distance(path.begin(), path.end()) ||
+               !equal(web_root_path.begin(), web_root_path.end(), path.begin()))
+                throw invalid_argument("path must be within root path");
+            if(boost::filesystem::is_directory(path))
+                path/="index.html";
+
+            std::string cache_control, etag;
+
+
+            auto ifs=make_shared<ifstream>();
+            ifs->open(path.string(), ifstream::in | ios::binary | ios::ate);
+            
+            if(*ifs) {
+                auto length=ifs->tellg();
+                ifs->seekg(0, ios::beg);
+                
+                *response << "HTTP/1.1 200 OK\r\n" << cache_control << etag << "Content-Length: " << length << "\r\n\r\n";
+                default_resource_send(server, response, ifs);
+            }
+            else
+                throw invalid_argument("could not read file");
+        }
+        catch(const exception &e) {
+            string content="Could not open path "+request->path+": "+e.what();
+            *response << "HTTP/1.1 400 Bad Request\r\nContent-Length: " << content.length() << "\r\n\r\n" << content;
+        }
+    };
+    
+    thread server_thread([&server](){
+        //Start server
+        server.start();
+    });
+    
+    //Wait for server to start so that the client can connect
+    this_thread::sleep_for(chrono::seconds(1));
+    
+    //Client examples
+    HttpClient client("localhost:8080");
+    auto r1=client.request("GET", "/match/123");
+    cout << r1->content.rdbuf() << endl;
+    cout << "SUCCESS" << endl;
+
+
+    server_thread.join();
+    
+    return 0;
+}
+
+void default_resource_send(const HttpServer &server, const shared_ptr<HttpServer::Response> &response,
+                           const shared_ptr<ifstream> &ifs) {
+    //read and send 128 KB at a time
+    static vector<char> buffer(131072); // Safe when server is running on one thread
+    streamsize read_length;
+    if((read_length=ifs->read(&buffer[0], buffer.size()).gcount())>0) {
+        response->write(&buffer[0], read_length);
+        if(read_length==static_cast<streamsize>(buffer.size())) {
+            server.send(response, [&server, response, ifs](const SimpleWeb::error_code &ec) {
+                if(!ec)
+                    default_resource_send(server, response, ifs);
+                else
+                    cerr << "Connection interrupted" << endl;
+            });
+        }
+    }
+}
